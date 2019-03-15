39cd92af90af4a41aa07237398e53731f7608f95
Dmitry Kulevich <dmitry.kulevich@capsilon.com>
2019-03-15 10:08:19 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stackOpenings = [];
+  var isCorrect = true;
+
+  outerloop:
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0] && !(stackOpenings[stackOpenings.length - 1] == str[i] && bracketsConfig[j][0] == bracketsConfig[j][1])) {
+        stackOpenings.push(str[i]);
+        break;
+      }
+      if (str[i] == bracketsConfig[j][1]) {
+        var bracket = stackOpenings.pop();
+        isCorrect = bracket == bracketsConfig[j][0];
+        if (!isCorrect) {
+          break outerloop;
+        }
+      }
+    }
+  }
+
+  return isCorrect && stackOpenings.length == 0;
 }
